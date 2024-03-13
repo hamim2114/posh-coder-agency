65944f63be4cee77e615cab0b80418a393991466
hamim <hamim2114@gmail.com>
2024-03-13 14:24:45 +0600

    updated

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -2,41 +2,41 @@ export const teams = [
   {
     name: 'Sabab Ashfak Fahim',
     title: 'Founder & CEO (Chief Executive Officer)',
-    img: '/public/teams/Sabab Ashfak Fahim.jpg'
+    img: '/teams/Sabab Ashfak Fahim.jpg'
   },
   {
     name: 'Mahin Sarker Bushra',
     title: 'Co-founder & COO (Chief Operating Officer)',
-    img: '/public/teams/Mahin Sarker Bushra.jpg'
+    img: '/teams/Mahin Sarker Bushra.jpg'
   },
   {
     name: 'Shahriar Zaman Nion',
     title: 'CMO (Chief Marketing Officer)',
-    img: '/public/teams/Shahriar Zaman Nion.jpg'
+    img: '/teams/Shahriar Zaman Nion.jpg'
   },
   {
     name: 'Shahed Mahmud',
     title: 'CFO (Chief Financial Officer)',
-    img: '/public/teams/Shahed Mahmud.jpg'
+    img: '/teams/Shahed Mahmud.jpg'
   },
   {
     name: 'Md. Tarek Mahmud',
     title: 'Software Developer',
-    img: '/public/teams/Md. Tarek Mahmud.jpg'
+    img: '/teams/Md. Tarek Mahmud.jpg'
   },
   {
     name: 'Salman Khan Fahim',
     title: ' Web Developer',
-    img: '/public/teams/Salman Khan Fahim.jpg'
+    img: '/teams/Salman Khan Fahim.jpg'
   },
   {
     name: 'Hamim Hossain',
     title: ' Web Developer',
-    img: '/public/teams/Hamim hossain.jpg'
+    img: '/teams/Hamim hossain.jpg'
   },
   {
     name: 'Sadman Sakib Roktim',
     title: ' Chief Graphics Designer',
-    img: '/public/teams/Sadman Sakib Roktim.jpg'
+    img: '/teams/Sadman Sakib Roktim.jpg'
   },
 ]
